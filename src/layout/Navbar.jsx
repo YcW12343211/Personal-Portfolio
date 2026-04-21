54b1dce63d19a23c87bea502baa2feeb7a384bf4
Yichao <nandubeiguistan@gmail.com>
2026-04-22 11:01:45 +1200

    Initial commit

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,29 @@
+
+
+const navLinks = [
+  { href: "#about", label: "About"},
+  { href: "#projects", label: "Projects" },
+  { href: "#experience", label: "Experience" },
+  { href: "#testimonials", label: "Testimonials"},
+];
+
+export const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <a>
+          PM<span>.</span>
+        </a>
+
+        {/*Desktop Nav*/}
+        <div>
+          <div>
+            {navLinks.map((link) => (
+              <a href={link.href}>{link.label}</a>
+            ))}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
